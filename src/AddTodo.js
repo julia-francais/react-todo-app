0994da5dc095029633221c400ca3dd615034b662
julia-francais <ju.francais@gmail.com>
2020-02-22 10:09:28 +0100

    todo app with delete and edit

---
+++ src/AddTodo.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+
+class AddTodo extends Component {
+    state= {
+        content: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            content: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.addTodo(this.state);
+        this.setState({
+            content: ''
+        })
+    }
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Add new todo</label>
+                    <input type="text" onChange={this.handleChange}></input>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddTodo
